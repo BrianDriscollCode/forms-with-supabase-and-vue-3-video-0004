7f8c961a8f531b73feec661c837ccb912f84e410
briandriscollcode <bdriscoll407@gmail.com>
2023-03-21 16:34:05 -0700

    Update: Added routing, user auth, and form handlers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { supabase } from '../clients/supabase'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import UnauthorizedView from '../views/UnauthorizedView.vue'
+
+let localUser;
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,17 +17,40 @@ const router = createRouter({
     {
       path: '/secret',
       name: 'secret',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SecretView.vue')
+      component: () => import('../views/SecretView.vue'),
+	  meta: { requiresAuth: true }
     },
 	{
       path: '/login',
 	  name: 'login',
 	  component: LoginView
+	},
+	{
+	  path: '/unauthorized',
+	  name: 'unauthorized',
+	  component: UnauthorizedView
 	}
   ]
+})
+
+async function getUser(next) {
+	localUser = await supabase.auth.getSession();
+	if (localUser.data.session == null) {
+		next('/unauthorized')
+	}
+	else {
+		next();
+	}
+}
+
+
+router.beforeEach((to, from, next) => {
+	if (to.meta.requiresAuth) {
+		getUser(next);
+	}
+	else {
+		next();
+	}
 })
 
 export default router
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,20 +2,87 @@
 	<h1> Test Form </h1>
 	<div class="inputContainer">
 		<label for="email"> Email: </label>
-		<input type="email" id="email">
+		<input type="email" id="email" v-model="email">
 	</div>
 
 	<div class="inputContainer">
 		<label for="email"> Password: </label>
-		<input type="password" id="password">
+		<input type="password" id="password" v-model="password">
+	</div>
+
+	<div class="inputContainer">
+		<label for="firstName"> First Name </label>
+		<input type="firstName" id="firstName" v-model="firstName">
 	</div>
 
 	<div class="buttonContainer">
-		<button> Create </button>
-		<button> Login </button>
-		<button> See user </button>
+		<button @click="createAccount"> Create </button>
+		<button @click="login"> Login </button>
+		<button @click="seeUser"> See user </button>
+		<button @click="logout"> Logout </button>
 	</div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { supabase } from "../clients/supabase"
+
+let email = ref("");
+let password = ref("");
+let firstName = ref("");
+
+async function createAccount() {
+	const { user, error } = await supabase.auth.signUp({
+		email: email.value,
+		password: password.value,
+		options: {
+			data: {
+				first_name: firstName.value
+			}
+		}
+	})
+	if (error)
+	{
+		console.log(error);
+	}
+	else
+	{
+		console.log(user);
+	}
+}
+
+async function login() {
+	console.log("run")
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email: email.value,
+		password: password.value
+	})
+	if (error)
+	{
+		console.log(error);
+	}
+	else
+	{
+		console.log(data);
+	}
+}
+
+async function seeUser() {
+	const localUser = await supabase.auth.getSession();
+	console.log(localUser.data.session)
+}
+
+async function logout() {
+	const { data, error } = await supabase.auth.signOut();
+
+	if (error) {
+		console.log(error);
+	}
+	else {
+        loggedUser.value = data;
+	}
+}
+</script>
 
 <style scoped>
 .inputContainer {
